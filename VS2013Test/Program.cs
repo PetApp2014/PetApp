de5052be18b7758fd6b3ac7f755710b64e02e4f6
PetApp2014 <chenwfy@gmail.com>
2014-04-21 12:01:02 +0800

    aaa

---
+++ VS2013Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VS2013Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
